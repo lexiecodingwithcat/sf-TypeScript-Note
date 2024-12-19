a18c64bd8eb4cfbf280730e7392233098ac75112
Tianzi Cui <cuitianzi0@gmail.com>
2024-12-18 22:28:01 -0700

    intersection type

--- app.js
+++ app.js
@@ -1,43 +1,6 @@
-//one class can implment many different interface
-var Person = /** @class */ (function () {
-    function Person(n) {
-        this.name = n;
-    }
-    Person.prototype.greet = function (phrase) {
-        console.log(phrase + " " + this.name);
-    };
-    return Person;
-}());
-//the type can be the same interface while the class can be different
-var user1;
-user1 = new Person('David');
-user1.greet('Hi, I am ');
-var currentSnow = /** @class */ (function () {
-    function currentSnow(loc, temp) {
-        this.location = loc;
-        this.tempareture = temp;
-    }
-    return currentSnow;
-}());
-var add;
-add = function (n1, n2) {
-    return n1 + n2;
+var e1 = {
+    name: "Max",
+    privileges: ["create-server"],
+    startDate: new Date()
 };
-var Toy = /** @class */ (function () {
-    function Toy(n) {
-        if (n) {
-            this.color = n;
-        }
-    }
-    Toy.prototype.greet = function (name) {
-        if (name) {
-            console.log("hi" + name);
-        }
-        else {
-            console.log('welcome!');
-        }
-    };
-    return Toy;
-}());
-var barbie = new Toy();
-barbie.greet('barbie');
+console.log(e1);
---
+++ app.ts
@@ -0,0 +1,21 @@
+// intersection type 
+type Admin = {
+    name:string,
+    privileges: string []
+};
+
+type Employee = {
+    name: string,
+    startDate: Date
+};
+type ElevatedEmployee = Admin & Employee;
+const e1 : ElevatedEmployee = {
+    name:"Max",
+    privileges:["create-server"],
+    startDate: new Date()
+}
+
+// console.log(e1);
+type Combinable = string | number ;
+type Numeric = number | boolean;
+type Universal = Combinable & Numeric; //can only store NUMBER
